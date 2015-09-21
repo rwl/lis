de59fdabb6a8c3f61528be67314996c5201fdb66
Richard Lincoln <r.w.lincoln@gmail.com>
2015-09-21 20:37:04 +0200

    Adding .js extensions to executables.

--- test/test.sh
+++ test/test.sh
@@ -18,7 +18,7 @@ if test "$enable_mpi" = "yes"; then
     MPIRUN="$MPIRUN $MPINP 2"
   fi
 else
-  MPIRUN=""
+  MPIRUN="nodejs"
 fi
 if test "$enable_omp" = "yes"; then
   OMP_NUM_THREADS=2
@@ -27,25 +27,25 @@ fi
 
 echo ' '
 echo 'checking linear solvers...'
-$MPIRUN $srcdir/test1 $srcdir/testmat.mtx 0 /dev/null /dev/null
+$MPIRUN $srcdir/test1.js $srcdir/testmat.mtx 0 /dev/null /dev/null
 
 echo 'checking eigensolvers...'
-$MPIRUN $srcdir/etest1 $srcdir/testmat.mtx /dev/null /dev/null
+$MPIRUN $srcdir/etest1.js $srcdir/testmat.mtx /dev/null /dev/null
 
 if test "$enable_fortran" = "yes" || test "$enable_f90" = "yes"; then
   echo 'checking Fortran interface...'
-  $MPIRUN $srcdir/test4f
+  $MPIRUN $srcdir/test4f.js
   echo ' '
 fi
 
 if test "$enable_quad" = "yes"; then
   echo 'checking double-double precision operations...'
-  $MPIRUN $srcdir/test5 200 2.0 -f double
-  $MPIRUN $srcdir/test5 200 2.0 -f quad
+  $MPIRUN $srcdir/test5.js 200 2.0 -f double
+  $MPIRUN $srcdir/test5.js 200 2.0 -f quad
 fi
 
 if test "$enable_saamg" = "yes"; then
   echo 'checking SAAMG preconditioner...'
-  $MPIRUN $srcdir/test2 10 10 1 /dev/null /dev/null -i cg -p saamg
+  $MPIRUN $srcdir/test2.js 10 10 1 /dev/null /dev/null -i cg -p saamg
 fi
 
